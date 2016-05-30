5fb0ae969814dcf9baaae54c96db3b7a7f1e9c8a
Chad Ellison <chad.ellison0123@gmail.com>
2016-05-30 13:42:46 -0600

    practice controller-tests

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,25 @@
+class ItemsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Item.all
+  end
+
+  def show
+    respond_with Item.find(params[:id])
+  end
+
+  def create
+    respond_with Item.create(item_params)
+  end
+
+  def update
+    respond_with Item.update(params[:id], item_params)
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description)
+    end
+end
---
+++ test/controllers/items_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class ItemsControllerTest < ActionController::TestCase
+  test '#index' do
+    get :index, format: :json
+
+    items = JSON.parse(response.body)
+    first_item = items.first
+
+    assert_response :success
+    assert_equal "Trump",           first_item["name"]
+    assert_equal "My hair is huge", first_item["description"]
+  end
+
+  test '#show' do
+
+    get :show, format: :json, id: "1"
+
+    item = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal "Trump", item["name"]
+    assert_equal "My hair is huge", item["description"]
+  end
+
+  test '#create' do
+    assert_difference("Item.count", 1) do
+      post :create, format: :json, item: { name: "Jones", description: "loves guac" }
+    end
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal "Jones",      item[:name]
+    assert_equal "loves guac", item[:description]
+  end
+
+  test '#update' do
+    old_item = Item.create(name: "Hillary", description: "You've got mail")
+
+    patch :update, format: :json, id: old_item.id, item: { name: "Bernie", description: "feel the Bern" }
+
+    # item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    # assert_equal "Jim", item[:name]
+    # assert_equal "this description", item[:description]
+  end
+end
